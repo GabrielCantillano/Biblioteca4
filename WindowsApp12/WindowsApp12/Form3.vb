a785c95b6cc97a402c4a18d30c0626786e2a9979
gabriel cantillano <gabrielcantillanoamaya_2003@outlook.com>
2019-11-02 18:34:52 -0600

    cambios

--- WindowsApp12/WindowsApp12/Form3.vb
+++ WindowsApp12/WindowsApp12/Form3.vb
@@ -11,4 +11,8 @@
         Me.AdministracionTableAdapter.Fill(Me.Administracion_BDDataSet.Administracion)
 
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
 End Class
